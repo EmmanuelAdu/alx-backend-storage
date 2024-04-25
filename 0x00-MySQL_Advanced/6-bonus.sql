79d25ba157d34d73a3900476d1328c782a797904
EmmanuelAdu <christorenzo20@gmail.com>
2024-04-25 23:06:13 +0000

    Solution to task7

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,13 @@
+-- Create a stored procedure that adds a new correction
+--   for a student
+
+DELIMITER $$
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
+		INSERT INTO projects (name) VALUES (project_name);
+	END IF;
+	INSERT INTO corrections (user_id, project_id, score)
+	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
+END;
+DELIMITER ;
