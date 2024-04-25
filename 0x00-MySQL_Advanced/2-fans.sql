463ce190b0a85f63abd2e6de4be71a748c9f49e3
EmmanuelAdu <christorenzo20@gmail.com>
2024-04-25 17:40:39 +0000

    Correction to nb_fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT origin, SUM(fans) AS total_fans
+SELECT origin, SUM(fans) AS nb_fans
     FROM metal_bands
     GROUP BY origin
-    ORDER BY total_fans DESC;
+    ORDER BY nb_fans DESC;
